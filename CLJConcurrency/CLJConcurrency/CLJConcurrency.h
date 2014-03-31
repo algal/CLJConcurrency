fc24fe1b9caef73f15eba86ed7450fdda822414f
Alexis Gallagher <alexis@alexisgallagher.com>
2014-03-30 21:06:42 -0700

    add project

---
+++ CLJConcurrency/CLJConcurrency/CLJConcurrency.h
@@ -0,0 +1,13 @@
+//
+//  CLJConcurrency.h
+//  CLJConcurrency
+//
+//  Created by Alexis Gallagher on 2014-03-30.
+//  Copyright (c) 2014 Bloom FIlter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLJConcurrency : NSObject
+
+@end
---
+++ CLJConcurrency/CLJConcurrency/CLJConcurrency.m
@@ -0,0 +1,13 @@
+//
+//  CLJConcurrency.m
+//  CLJConcurrency
+//
+//  Created by Alexis Gallagher on 2014-03-30.
+//  Copyright (c) 2014 Bloom FIlter. All rights reserved.
+//
+
+#import "CLJConcurrency.h"
+
+@implementation CLJConcurrency
+
+@end
