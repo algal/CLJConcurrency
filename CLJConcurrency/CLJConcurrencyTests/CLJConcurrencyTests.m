fb2b23a558c337776bc86341f7e4db51f22210c7
Alexis Gallagher <alexis@alexisgallagher.com>
2014-03-30 22:53:04 -0700

    add tests

--- CLJConcurrency/CLJConcurrencyTests/CLJConcurrencyTests.m
+++ CLJConcurrency/CLJConcurrencyTests/CLJConcurrencyTests.m
@@ -28,15 +28,57 @@
     [super tearDown];
 }
 
-- (void) testFuture
+- (void) testFutureValue
 {
   CLJFuture * futureSum = [[CLJFuture alloc] initWithBlock:^id{
     return @(40 +2);
   }];
-  
-  XCTAssertEqualObjects(@(42), futureSum.value, @"sums interested");
-  
+  XCTAssertEqualObjects(@(42), futureSum.value, @"sums");
 }
 
+- (void) testIsRealized
+{
+  CLJFuture * delayedValue = [[CLJFuture alloc] initWithBlock:^id{
+    sleep(1);
+    return @(1);
+  }];
+  XCTAssertEqual(NO, delayedValue.isRealized, @"not realized");
+  sleep(2);
+  XCTAssertEqual(YES, delayedValue.isRealized, @"not realized");
+  XCTAssertEqualObjects(@(1),delayedValue.value,@"sums2");
+}
+
+- (void) testAsyncOrdering
+{
+  __block NSMutableArray * results = [NSMutableArray array];
+  CLJFuture * delayedSum = [[CLJFuture alloc] initWithBlock:^id{
+    sleep(1);
+    [results addObject:@"second"];
+    return nil;
+  }];
+  XCTAssertEqual(NO, delayedSum.isRealized, @"not realized");
+  [results addObject:@"first"];
+  sleep(2);
+  XCTAssertEqual(YES, delayedSum.isRealized, @"not realized");
+
+  NSArray * expected = @[@"first",@"second"];
+  XCTAssertEqualObjects(expected,results,@"sums2");
+}
+
+- (void) testDerefBlocks
+{
+  CFAbsoluteTime const start = CFAbsoluteTimeGetCurrent();
+  
+  CLJFuture *  v = [[CLJFuture alloc] initWithBlock:^id{
+    sleep(3);
+    return @(1);
+  }];
+  
+  NSNumber * n = v.value;
+  CFAbsoluteTime const end = CFAbsoluteTimeGetCurrent();
+  CFTimeInterval interval = end-start;
+  XCTAssert(interval >= (CFTimeInterval)3.0, @"value deref delayed execution");
+  XCTAssertEqualObjects(@(1), n, @"future value");
+}
 
 @end
